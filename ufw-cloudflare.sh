0e5b41e4a6f9a370f7f3c4b768a8945ecc87d8b6
UserX <7280295+GeorgePatsias@users.noreply.github.com>
2022-07-13 22:40:27 +0300

    Update ufw-cloudflare.sh

--- ufw-cloudflare.sh
+++ ufw-cloudflare.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-curl -s https://www.cloudflare.com/ips-v4 -o /tmp/cf_ips
-echo "" >> /tmp/cf_ips
-curl -s https://www.cloudflare.com/ips-v6 >> /tmp/cf_ips
+curl -s https://www.cloudflare.com/ips-v4 -o /tmp/cloudflare_ips
+echo "" >> /tmp/cloudflare_ips
+curl -s https://www.cloudflare.com/ips-v6 >> /tmp/cloudflare_ips
 
 # Allow all traffic from Cloudflare IPs (all ports).
-for cfip in `cat /tmp/cf_ips`; do ufw allow proto tcp from $cfip comment 'Cloudflare IP'; done
+for ip in `cat /tmp/cloudflare_ips`; do ufw allow proto tcp from $ip comment 'Cloudflare IP'; done
 
 ufw reload > /dev/null
+
+rm /tmp/cloudflare_ips
 
 # OTHER EXAMPLE RULES
 # Allow on port 80
